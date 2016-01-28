4b67f65d52de453c87f0f2a677260eadc4b2979d
Johan Van Der Werff <johanvdw@zazooltd.com>
2016-01-28 16:02:06 +0200

    more

--- SimpleTable/SimpleTableViewController.h
+++ SimpleTable/SimpleTableViewController.h
@@ -6,6 +6,11 @@
 //  Copyright (c) 2012 AppCoda. All rights reserved.
 //
 
+
+//
+//
+
+
 #import <UIKit/UIKit.h>
 
 @interface SimpleTableViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
